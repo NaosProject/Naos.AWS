006126a77cf46772397b2b16eb67ed985510f3c9
Lawson Caudill <lawson@caudillclan.us>
2015-09-03 12:16:32 -0400

    Added CloudFile and IManagerFiles to contract, added FileManager (implementation of IManageFiles) to Core.  Update to latest AWSSDK.

---
+++ Naos.AWS.Core/FileManager.cs
@@ -0,0 +1,130 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FileManager.cs" company="Naos">
+//   Copyright 2015 Naos
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.AWS.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Amazon;
+    using Amazon.S3;
+    using Amazon.S3.Model;
+    using Amazon.S3.Transfer;
+
+    using Naos.AWS.Contract;
+
+    /// <summary>
+    /// Class to operate against S3.
+    /// </summary>
+    public class FileManager : IManageFiles
+    {
+        private readonly string accessKey;
+
+        private readonly string secretKey;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileManager"/> class.
+        /// </summary>
+        /// <param name="accessKey">Access key with rights to download files in specified buckets.</param>
+        /// <param name="secretKey">Secret key with rights to download files in specified buckets.</param>
+        public FileManager(string accessKey, string secretKey)
+        {
+            this.accessKey = accessKey;
+            this.secretKey = secretKey;
+        }
+
+        /// <inheritdoc />
+        public void UploadFile(string region, string bucketName, string keyName, string sourceFilePath)
+        {
+            var regionEndpoint = RegionEndpoint.GetBySystemName(region);
+            using (var client = new AmazonS3Client(this.accessKey, this.secretKey, regionEndpoint))
+            {
+                using (var transferUtility = new TransferUtility(client))
+                {
+                    transferUtility.Upload(sourceFilePath, bucketName, keyName);
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        public Task UploadFileAsync(string region, string bucketName, string keyName, string sourceFilePath)
+        {
+            var regionEndpoint = RegionEndpoint.GetBySystemName(region);
+            using (var client = new AmazonS3Client(this.accessKey, this.secretKey, regionEndpoint))
+            {
+                using (var transferUtility = new TransferUtility(client))
+                {
+                    return transferUtility.UploadAsync(sourceFilePath, bucketName, keyName);
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        public void DownloadFile(string region, string bucketName, string keyName, string destinationFilePath)
+        {
+            var regionEndpoint = RegionEndpoint.GetBySystemName(region);
+            using (var client = new AmazonS3Client(this.accessKey, this.secretKey, regionEndpoint))
+            {
+                using (var transferUtility = new TransferUtility(client))
+                {
+                    transferUtility.Download(destinationFilePath, bucketName, keyName);
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        public Task DownloadFileAsync(string region, string bucketName, string keyName, string destinationFilePath)
+        {
+            var regionEndpoint = RegionEndpoint.GetBySystemName(region);
+            using (var client = new AmazonS3Client(this.accessKey, this.secretKey, regionEndpoint))
+            {
+                using (var transferUtility = new TransferUtility(client))
+                {
+                    return transferUtility.DownloadAsync(destinationFilePath, bucketName, keyName);
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        public ICollection<CloudFile> ListFiles(string region, string bucketName)
+        {
+            return this.ListFiles(region, bucketName, null);
+        }
+
+        /// <inheritdoc />
+        public ICollection<CloudFile> ListFiles(string region, string bucketName, string keyPrefixSearchPattern)
+        {
+            var regionEndpoint = RegionEndpoint.GetBySystemName(region);
+            using (var client = new AmazonS3Client(this.accessKey, this.secretKey, regionEndpoint))
+            {
+                var request = new ListObjectsRequest { BucketName = bucketName, Prefix = keyPrefixSearchPattern };
+
+                var objects = client.ListObjects(request);
+                var ret = new List<CloudFile>();
+                if (objects != null && objects.S3Objects != null && objects.S3Objects.Count > 0)
+                {
+                    ret.AddRange(
+                        objects.S3Objects.Select(
+                            _ =>
+                            new CloudFile
+                                {
+                                    Region = region,
+                                    BucketName = bucketName,
+                                    KeyName = _.Key,
+                                    OwnerId = _.Owner.Id,
+                                    OwnerName = _.Owner.DisplayName,
+                                    LastModified = _.LastModified,
+                                    Size = _.Size
+                                }));
+                }
+
+                return ret;
+            }
+        }
+    }
+}
