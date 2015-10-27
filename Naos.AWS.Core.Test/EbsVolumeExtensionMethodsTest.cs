68d87b0cd5d841494f83f02448c3c43327d9582f
Lawson Caudill <lawson@caudillclan.us>
2015-10-27 19:04:07 -0300

    Added logic to support provisioned iops as well as specific iops by using io1-###; will default to max of 30:1 iops:gb.

--- Naos.AWS.Core.Test/EbsVolumeExtensionMethodsTest.cs
+++ Naos.AWS.Core.Test/EbsVolumeExtensionMethodsTest.cs
@@ -6,6 +6,7 @@
 
 namespace Naos.AWS.Core.Test
 {
+    using System;
     using System.Linq;
 
     using Naos.AWS.Contract;
@@ -39,6 +40,118 @@ namespace Naos.AWS.Core.Test
             Assert.Equal(volumes.First().SizeInGb, blockMappings.First().Ebs.VolumeSize);
             Assert.Equal(volumes.First().VolumeType, blockMappings.First().Ebs.VolumeType);
             Assert.Equal(true, blockMappings.First().Ebs.DeleteOnTermination);
+        }
+
+        [Fact]
+        public static void ToAwsBlockDeviceMappings_EbsVolumeWithIopsAndNoLevelSpecified_GetsThirtyToOne()
+        {
+            var volumes = new[]
+                              {
+                                  new EbsVolume()
+                                      {
+                                          DeviceName = "MonkeysDevice",
+                                          Id = "ebs--someId",
+                                          Name = "IamTheEbsVolume",
+                                          Region = "us-south-42",
+                                          SizeInGb = 1000,
+                                          VirtualName = "OS",
+                                          VolumeType = "io1"
+                                      }
+                              }.ToList();
+
+            var blockMappings = volumes.ToAwsBlockDeviceMappings();
+
+            Assert.Equal(volumes.Single().VolumeType, blockMappings.Single().Ebs.VolumeType);
+            Assert.Equal(volumes.Single().SizeInGb * 30, blockMappings.Single().Ebs.Iops);
+        }
+
+        [Fact]
+        public static void ToAwsBlockDeviceMappings_EbsVolumeWithIopsAndLevelSpecified_GetsSpecified()
+        {
+            var volumes = new[]
+                              {
+                                  new EbsVolume()
+                                      {
+                                          DeviceName = "MonkeysDevice",
+                                          Id = "ebs--someId",
+                                          Name = "IamTheEbsVolume",
+                                          Region = "us-south-42",
+                                          SizeInGb = 150,
+                                          VirtualName = "OS",
+                                          VolumeType = "io1-4000"
+                                      }
+                              }.ToList();
+
+            var blockMappings = volumes.ToAwsBlockDeviceMappings();
+
+            Assert.Equal("io1", blockMappings.Single().Ebs.VolumeType);
+            Assert.Equal(4000, blockMappings.Single().Ebs.Iops);
+        }
+
+        [Fact]
+        public static void ToAwsBlockDeviceMappings_EbsVolumeWithIopsAndLevelSpecifiedOverThirtyToOne_Throws()
+        {
+            var volumes = new[]
+                              {
+                                  new EbsVolume()
+                                      {
+                                          DeviceName = "MonkeysDevice",
+                                          Id = "ebs--someId",
+                                          Name = "IamTheEbsVolume",
+                                          Region = "us-south-42",
+                                          SizeInGb = 1,
+                                          VirtualName = "OS",
+                                          VolumeType = "io1-300"
+                                      }
+                              }.ToList();
+
+            Action testCode = () => volumes.ToAwsBlockDeviceMappings();
+            var ex = Assert.Throws<ArgumentException>(testCode);
+            Assert.Equal("Specified IOPS: 300 was greated than allowed (30 IOPS:1GB): 30", ex.Message);
+        }
+
+        [Fact]
+        public static void ToAwsBlockDeviceMappings_EbsVolumeWithGp2_GetsZeroIops()
+        {
+            var volumes = new[]
+                              {
+                                  new EbsVolume()
+                                      {
+                                          DeviceName = "MonkeysDevice",
+                                          Id = "ebs--someId",
+                                          Name = "IamTheEbsVolume",
+                                          Region = "us-south-42",
+                                          SizeInGb = 1,
+                                          VirtualName = "OS",
+                                          VolumeType = "gp2"
+                                      }
+                              }.ToList();
+
+            var mappings = volumes.ToAwsBlockDeviceMappings();
+            Assert.Equal(volumes.Single().VolumeType, mappings.Single().Ebs.VolumeType);
+            Assert.Equal(0, mappings.Single().Ebs.Iops);
+        }
+
+        [Fact]
+        public static void ToAwsBlockDeviceMappings_EbsVolumeWithStandard_GetsZeroIops()
+        {
+            var volumes = new[]
+                              {
+                                  new EbsVolume()
+                                      {
+                                          DeviceName = "MonkeysDevice",
+                                          Id = "ebs--someId",
+                                          Name = "IamTheEbsVolume",
+                                          Region = "us-south-42",
+                                          SizeInGb = 1,
+                                          VirtualName = "OS",
+                                          VolumeType = "standard"
+                                      }
+                              }.ToList();
+
+            var mappings = volumes.ToAwsBlockDeviceMappings();
+            Assert.Equal(volumes.Single().VolumeType, mappings.Single().Ebs.VolumeType);
+            Assert.Equal(0, mappings.Single().Ebs.Iops);
         }
     }
 }
