afa07c71e95dbdd5f3fae8cae1abd7742ba07f55
randylevy <randy.levy@gmail.com>
2017-01-27 03:03:09 -0500

    Move constructor to the top and Must validate parameters.

--- Naos.AWS.S3/Domain/ComputedChecksum.cs
+++ Naos.AWS.S3/Domain/ComputedChecksum.cs
@@ -16,16 +16,6 @@ namespace Naos.AWS.S3
     public class ComputedChecksum
     {
         /// <summary>
-        /// Gets the name of the hash algorithm used to compute the checksum.
-        /// </summary>
-        public HashAlgorithmName HashAlgorithmName { get; private set; }
-
-        /// <summary>
-        /// Gets the value of the computed checksum.
-        /// </summary>
-        public string Value { get; private set; }
-
-        /// <summary>
         /// Initializes a new instance of the <see cref="ComputedChecksum"/> class.
         /// </summary>
         /// <param name="hashAlgorithmName">The name of the hash algorithm name to used to compute the checksum.</param>
@@ -35,5 +25,15 @@ namespace Naos.AWS.S3
             this.HashAlgorithmName = hashAlgorithmName;
             this.Value = hashValue;
         }
+
+        /// <summary>
+        /// Gets the name of the hash algorithm used to compute the checksum.
+        /// </summary>
+        public HashAlgorithmName HashAlgorithmName { get; private set; }
+
+        /// <summary>
+        /// Gets the value of the computed checksum.
+        /// </summary>
+        public string Value { get; private set; }
     }
 }
--- Naos.AWS.S3/UploadFileResult.cs
+++ Naos.AWS.S3/UploadFileResult.cs
@@ -6,11 +6,36 @@
 
 namespace Naos.AWS.S3
 {
+    using System.Collections.Generic;
+    using System.Security.Cryptography;
+
+    using Spritely.Recipes;
+
     /// <summary>
     /// Result of uploading a file to S3.
     /// </summary>
     public class UploadFileResult
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UploadFileResult"/> class.
+        /// </summary>
+        /// <param name="region">Region bucket is in.</param>
+        /// <param name="bucketName">Bucket name file was uploaded to.</param>
+        /// <param name="keyName">Key name of the file.</param>
+        /// <param name="computedChecksums">Computed checksums of the uploaded file.</param>
+        public UploadFileResult(string region, string bucketName, string keyName, IReadOnlyDictionary<HashAlgorithmName, ComputedChecksum> computedChecksums)
+        {
+            region.Named(nameof(region)).Must().NotBeWhiteSpace().OrThrow();
+            bucketName.Named(nameof(bucketName)).Must().NotBeWhiteSpace().OrThrow();
+            keyName.Named(nameof(keyName)).Must().NotBeWhiteSpace().OrThrow();
+            computedChecksums.Named(nameof(computedChecksums)).Must().NotBeNull().OrThrow();
+
+            this.Region = region;
+            this.BucketName = bucketName;
+            this.KeyName = keyName;
+            this.Checksums = computedChecksums;
+        }
+
         /// <summary>
         /// Gets the region the file was uploaded to.
         /// </summary>
@@ -27,23 +52,8 @@ namespace Naos.AWS.S3
         public string KeyName { get; private set; }
 
         /// <summary>
-        /// Gets the checksum of the uploaded file.
+        /// Gets the checksums for the uploaded file.
         /// </summary>
-        public ComputedChecksum Checksum { get; private set; }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="UploadFileResult"/> class.
-        /// </summary>
-        /// <param name="region">Region bucket is in.</param>
-        /// <param name="bucketName">Bucket name file was uploaded to.</param>
-        /// <param name="keyName">Key name of the file.</param>
-        /// <param name="computedChecksum">Checksum of the uploaded file.</param>
-        public UploadFileResult(string region, string bucketName, string keyName, ComputedChecksum computedChecksum)
-        {
-            this.Region = region;
-            this.BucketName = bucketName;
-            this.KeyName = keyName;
-            this.Checksum = computedChecksum;
-        }
+        public IReadOnlyDictionary<HashAlgorithmName, ComputedChecksum> Checksums { get; private set; }
     }
 }
