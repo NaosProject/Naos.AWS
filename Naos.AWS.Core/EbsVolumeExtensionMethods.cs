9685a2a3138af9ee21e1cc89632d884d0926f92c
Lawson Caudill <lawson@getthinktank.com>
2022-12-08 13:14:12 -0500

    Added CreateVolume and AttachToInstance methods to create new volumes and attach to an instance after the fact.

--- Naos.AWS.Core/EbsVolumeExtensionMethods.cs
+++ Naos.AWS.Core/EbsVolumeExtensionMethods.cs
@@ -104,5 +104,94 @@ namespace Naos.AWS.Core
                 return response.Volumes.Any(_ => _.VolumeId == volume.Id);
             }
         }
+
+        /// <summary>
+        /// Creates the volume.
+        /// </summary>
+        /// <param name="volume">The volume.</param>
+        /// <param name="availabilityZone">Availability zone of subnet.</param>
+        /// <param name="timeout">Optional timeout to wait until object exists; DEFAULT is ininity.</param>
+        /// <param name="credentials">Credentials to use (will use the credentials from CredentialManager.Cached if null...).</param>
+        /// <returns>Updated copy of the provided object.</returns>
+        public static async Task<EbsVolume> CreateVolume(
+            this EbsVolume volume,
+            string availabilityZone,
+            TimeSpan timeout,
+            CredentialContainer credentials = null)
+        {
+            var awsCredentials = CredentialManager.GetAwsCredentials(credentials);
+            var regionEndpoint = RegionEndpoint.GetBySystemName(volume.Region);
+
+            var localVolume = volume.DeepClone();
+            var volumeType = volume.VolumeType;
+
+            using (var client = new AmazonEC2Client(awsCredentials, regionEndpoint))
+            {
+                var request = new CreateVolumeRequest() { AvailabilityZone = availabilityZone, Size = volume.SizeInGb, };
+                if (volume.VolumeType.StartsWith("io1", StringComparison.OrdinalIgnoreCase))
+                {
+                    var dashSplit = volume.VolumeType.Split('-');
+                    volumeType = dashSplit[0];
+                    int iops;
+                    var maxIops = volume.SizeInGb * 30;
+                    if (dashSplit.Length == 1)
+                    {
+                        iops = maxIops;
+                    }
+                    else
+                    {
+                        iops = int.Parse(dashSplit[1], CultureInfo.InvariantCulture);
+                    }
+
+                    if (iops > maxIops)
+                    {
+                        throw new ArgumentException(FormattableString.Invariant($"Specified IOPS: {iops} was greated than allowed (30 IOPS:1GB): {maxIops}"));
+                    }
+
+                    request.Iops = iops;
+                }
+
+                request.VolumeType = volumeType;
+
+                var response = await client.CreateVolumeAsync(request);
+                Validator.ThrowOnBadResult(request, response);
+
+                await localVolume.TagNameInAwsAsync(timeout, credentials);
+
+                localVolume.Id = response.Volume.VolumeId;
+                return localVolume;
+            }
+        }
+
+        /// <summary>
+        /// Attaches to instance.
+        /// </summary>
+        /// <param name="volume">The volume.</param>
+        /// <param name="instanceId">The instance identifier.</param>
+        /// <param name="device">The device.</param>
+        /// <param name="credentials">Credentials to use (will use the credentials from CredentialManager.Cached if null...).</param>
+        /// <returns>Task for async.</returns>
+        public static async Task AttachToInstance(
+            this EbsVolume volume,
+            string instanceId,
+            string device,
+            CredentialContainer credentials = null)
+        {
+            var awsCredentials = CredentialManager.GetAwsCredentials(credentials);
+            var regionEndpoint = RegionEndpoint.GetBySystemName(volume.Region);
+
+            using (var client = new AmazonEC2Client(awsCredentials, regionEndpoint))
+            {
+                var request = new AttachVolumeRequest()
+                              {
+                                  Device = device,
+                                  InstanceId = instanceId,
+                                  VolumeId = volume.Id,
+                              };
+
+                var response = await client.AttachVolumeAsync(request);
+                Validator.ThrowOnBadResult(request, response);
+            }
+        }
     }
 }
