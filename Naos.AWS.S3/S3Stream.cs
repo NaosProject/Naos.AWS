5bbe394ba4401b74ed52ff945c2edb2883ac9551
Suraj Gupta <SurajGupta@users.noreply.github.com>
2025-01-28 14:17:47 -0500

    S3Stream.StandardGetLatestRecordOp enhancements
    better argument checking, support RecordNotFoundStrategy, support Tags, support StreamRecordItemsToInclude, use LastModified date/time for timestamp instead of DateTime.UtcNow

--- Naos.AWS.S3/S3Stream.cs
+++ Naos.AWS.S3/S3Stream.cs
@@ -16,13 +16,13 @@ namespace Naos.AWS.S3
     using Naos.AWS.Domain;
     using Naos.CodeAnalysis.Recipes;
     using Naos.Database.Domain;
-
     using OBeautifulCode.Assertion.Recipes;
     using OBeautifulCode.Execution.Recipes;
     using OBeautifulCode.Representation.System;
     using OBeautifulCode.Serialization;
     using OBeautifulCode.String.Recipes;
     using OBeautifulCode.Type;
+    using OBeautifulCode.Type.Recipes;
     using static System.FormattableString;
 
     /// <summary>
@@ -32,7 +32,16 @@ namespace Naos.AWS.S3
     [SuppressMessage("Microsoft.Naming", "CA1711:IdentifiersShouldNotHaveIncorrectSuffix", Justification = NaosSuppressBecause.CA1711_IdentifiersShouldNotHaveIncorrectSuffix_TypeNameAddedAsSuffixForTestsWhereTypeIsPrimaryConcern)]
     public class S3Stream : StandardStreamBase
     {
+        private const string MetadataKeyTagNamePrefix = "tag-";
+        private static readonly TypeRepresentation IdentifierTypeRepresentation = typeof(string).ToRepresentation();
+        private static readonly TypeRepresentation ObjectTypeRepresentation = typeof(byte[]).ToRepresentation();
+        private static readonly TypeRepresentation IdentifierTypeRepresentationWithoutAssemblyVersions = IdentifierTypeRepresentation.RemoveAssemblyVersions();
+        private static readonly TypeRepresentation ObjectTypeRepresentationWithoutAssemblyVersions = ObjectTypeRepresentation.RemoveAssemblyVersions();
+        private static readonly TypeRepresentationWithAndWithoutVersion IdentifierTypeRepresentationWithAndWithoutVersion = IdentifierTypeRepresentation.ToWithAndWithoutVersion();
+        private static readonly TypeRepresentationWithAndWithoutVersion ObjectTypeRepresentationWithAndWithoutVersion = ObjectTypeRepresentation.ToWithAndWithoutVersion();
+
         private readonly IManageFiles fileManager;
+        private readonly IStringSerializeAndDeserialize tagSerializer;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="S3Stream"/> class.
@@ -43,18 +52,26 @@ namespace Naos.AWS.S3
         /// <param name="defaultSerializationFormat">The default serialization format.</param>
         /// <param name="resourceLocatorProtocols">Protocol to get appropriate resource locator(s).</param>
         /// <param name="fileManager">Backing interface to interact with S3.</param>
+        /// <param name="tagSerializer">The serializer to use when serializing tags.</param>
         public S3Stream(
             string name,
             ISerializerFactory serializerFactory,
             SerializerRepresentation defaultSerializerRepresentation,
             SerializationFormat defaultSerializationFormat,
             IResourceLocatorProtocols resourceLocatorProtocols,
-            IManageFiles fileManager)
+            IManageFiles fileManager,
+            IStringSerializeAndDeserialize tagSerializer)
             : base(name, serializerFactory, defaultSerializerRepresentation, defaultSerializationFormat, resourceLocatorProtocols)
         {
             fileManager.MustForArg(nameof(fileManager)).NotBeNull();
+            fileManager.MustForArg(nameof(tagSerializer)).NotBeNull();
+
             this.fileManager = fileManager;
+            this.tagSerializer = tagSerializer;
         }
+
+        /// <inheritdoc />
+        public override IStreamRepresentation StreamRepresentation => new StreamRepresentation(this.Name);
 
         /// <inheritdoc />
         public override long Execute(
@@ -68,65 +85,96 @@ namespace Naos.AWS.S3
         public override StreamRecord Execute(
             StandardGetLatestRecordOp operation)
         {
-            var identifierTypeRepresentation = typeof(string).ToRepresentation();
-            var objectTypeRepresentation = typeof(byte[]).ToRepresentation();
+            // All of the GetAll... operations and the GetLatestObjects (plural, not singular) use internal record ids.
+            // So an S3 stream cannot be used for those operations.
+            operation.RecordFilter.InternalRecordIds.MustForArg(Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.InternalRecordIds)}")).BeNull(Invariant($"No support for {nameof(RecordFilter.InternalRecordIds)}."));
+            operation.RecordFilter.Ids.MustForArg(Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.Ids)}")).NotBeNullNorEmptyEnumerableNorContainAnyNulls().And().HaveCount(1, Invariant($"{nameof(RecordFilter.Ids)} must be specified, only one is supported, and it must be a {typeof(string).ToStringReadable()}."));
+            operation.RecordFilter.Ids.Single().IdentifierType.RemoveAssemblyVersions().MustForArg(Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.Ids)}.Single().{nameof(StringSerializedIdentifier.IdentifierType)}")).BeEqualTo(IdentifierTypeRepresentationWithoutAssemblyVersions, Invariant($"{nameof(RecordFilter.Ids)} must be specified, only one is supported, and it must be a {typeof(string).ToStringReadable()}."));
 
-            operation.RecordNotFoundStrategy.MustForArg(Invariant($"{nameof(operation)}.{nameof(operation.RecordNotFoundStrategy)}"))
-                     .BeEqualTo(RecordNotFoundStrategy.ReturnDefault);
-            operation.RecordFilter.DeprecatedIdTypes.MustForArg(
-                          Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.DeprecatedIdTypes)}"))
-                     .BeNull();
-            operation.RecordFilter.IdTypes.MustForArg(
-                          Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.IdTypes)}"))
-                     .BeNull();
-            operation.RecordFilter.InternalRecordIds.MustForArg(
-                          Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.InternalRecordIds)}"))
-                     .BeNull();
-            operation.RecordFilter.ObjectTypes.Single().MustForArg(
-                          Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.ObjectTypes)}"))
-                     .BeEqualTo(objectTypeRepresentation);
-            operation.RecordFilter.Tags.MustForArg(
-                          Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.Tags)}"))
-                     .BeNull();
-            operation.RecordFilter.Ids.MustForArg(
-                          Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.Ids)}"))
-                     .NotBeEmptyEnumerable().And().HaveCount(1);
+            // Looking at Naos.Database, none of the operations that filter on id (e.g. GetLatestObjectByIdOp<TId, TObject>) also filter on id type.
+            // The only ones that filter on id type are GetLatestObjectOp<TObject> and GetLatestRecordOp<TObject> and those don't specify id which is required.
+            operation.RecordFilter.IdTypes.MustForArg(Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.IdTypes)}")).BeNull(Invariant($"No support for {nameof(RecordFilter.IdTypes)}."));
 
-            var id = operation.RecordFilter.Ids.Select(
-                                   _ =>
-                                   {
-                                       _.IdentifierType.RemoveAssemblyVersions()
-                                        .MustForArg(
-                                             Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.Ids)}"))
-                                        .BeEqualTo(identifierTypeRepresentation.RemoveAssemblyVersions());
-                                       return _.StringSerializedId;
-                                   })
-                              .Single();
+            operation.RecordFilter.ObjectTypes.MustForArg(Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.ObjectTypes)}")).NotBeNullNorEmptyEnumerableNorContainAnyNulls().And().HaveCount(1, Invariant($"{nameof(RecordFilter.ObjectTypes)} must be specified, only one is supported, and it must be a {typeof(byte[]).ToStringReadable()}."));
+            operation.RecordFilter.ObjectTypes.Single().RemoveAssemblyVersions().MustForArg(Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.ObjectTypes)}.Single()")).BeEqualTo(ObjectTypeRepresentationWithoutAssemblyVersions, Invariant($"{nameof(RecordFilter.ObjectTypes)} must be specified, only one is supported, and it must be a {typeof(byte[]).ToStringReadable()}."));
+            operation.RecordFilter.VersionMatchStrategy.MustForArg(Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.VersionMatchStrategy)}")).BeEqualTo(VersionMatchStrategy.Any, Invariant($"The only supported {nameof(RecordFilter.VersionMatchStrategy)} is {nameof(VersionMatchStrategy.Any)}."));
+            operation.RecordFilter.Tags.MustForArg(Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.Tags)}")).BeNull(Invariant($"No support for {nameof(RecordFilter.Tags)} to filter on."));
+            operation.RecordFilter.DeprecatedIdTypes.MustForArg(Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.DeprecatedIdTypes)}")).BeNull(Invariant($"No support for {nameof(RecordFilter.DeprecatedIdTypes)}."));
+
+            var id = operation.RecordFilter.Ids.Single().StringSerializedId;
 
             var resourceLocator = this.TryGetSingleLocator(operation);
 
+            bool throwIfKeyNotFound;
+            switch (operation.RecordNotFoundStrategy)
+            {
+                case RecordNotFoundStrategy.ReturnDefault:
+                    throwIfKeyNotFound = false;
+                    break;
+                case RecordNotFoundStrategy.Throw:
+                    throwIfKeyNotFound = true;
+                    break;
+                default:
+                    throw new NotSupportedException(Invariant($"This {nameof(RecordNotFoundStrategy)} is not supported: {operation.RecordNotFoundStrategy}."));
+            }
+
             using (var destinationStream = new MemoryStream())
             {
-                Func<Task> downloadFileAsyncFunc = () => this.fileManager
-                                                             .DownloadFileAsync(
-                                                                  resourceLocator.Region,
-                                                                  resourceLocator.BucketName,
-                                                                  id,
-                                                                  destinationStream);
+                // ReSharper disable once AccessToDisposedClosure
+                Func<Task<DownloadFileResult>> downloadFileAsyncFunc = () => this.fileManager
+                    .DownloadFileAsync(
+                        resourceLocator.Region,
+                        resourceLocator.BucketName,
+                        id,
+                        destinationStream,
+                        validateChecksumsIfPresent: true,
+                        throwIfKeyNotFound);
 
-                downloadFileAsyncFunc.ExecuteSynchronously();
-                var resultMetadata = new StreamRecordMetadata(
-                    id,
-                    this.DefaultSerializerRepresentation,
-                    identifierTypeRepresentation.ToWithAndWithoutVersion(),
-                    objectTypeRepresentation.ToWithAndWithoutVersion(),
-                    new NamedValue<string>[0],
-                    DateTime.UtcNow,
-                    null);
-                var result = new StreamRecord(
-                    -1,
-                    resultMetadata,
-                    new BinaryDescribedSerialization(objectTypeRepresentation, this.DefaultSerializerRepresentation, destinationStream.ToArray()));
+                var downloadFileResult = downloadFileAsyncFunc.ExecuteSynchronously();
+
+                StreamRecord result;
+
+                if (downloadFileResult.KeyExists)
+                {
+                    var tags = this.GetTags(downloadFileResult.UserDefinedMetadata);
+
+                    // ReSharper disable once PossibleInvalidOperationException - LastModifiedUtc guaranteed to not be null when KeyExists == true
+                    var resultMetadata = new StreamRecordMetadata(
+                        id,
+                        this.DefaultSerializerRepresentation,
+                        IdentifierTypeRepresentationWithAndWithoutVersion,
+                        ObjectTypeRepresentationWithAndWithoutVersion,
+                        tags,
+                        (DateTime)downloadFileResult.LastModifiedUtc,
+                        null); // the object is a byte array so it doesn't implement IHaveTimestampUtc
+
+                    DescribedSerializationBase describedSerializationBase;
+
+                    switch (operation.StreamRecordItemsToInclude)
+                    {
+                        case StreamRecordItemsToInclude.MetadataOnly:
+                            describedSerializationBase = new NullDescribedSerialization(
+                                resultMetadata.TypeRepresentationOfObject.WithVersion,
+                                resultMetadata.SerializerRepresentation);
+                            break;
+                        case StreamRecordItemsToInclude.MetadataAndPayload:
+                            describedSerializationBase = new BinaryDescribedSerialization(
+                                resultMetadata.TypeRepresentationOfObject.WithVersion,
+                                resultMetadata.SerializerRepresentation,
+                                destinationStream.ToArray());
+                            break;
+                        default:
+                            throw new NotSupportedException(Invariant($"This {nameof(StreamRecordItemsToInclude)} is not supported: {operation.StreamRecordItemsToInclude}."));
+                    }
+
+                    result = new StreamRecord(-1, resultMetadata, describedSerializationBase);
+                }
+                else
+                {
+                    // Key doesn't exist and we haven't thrown per throwIfKeyNotFound, so RecordNotFoundStrategy is ReturnDefault
+                    result = null;
+                }
+
                 return result;
             }
         }
@@ -226,33 +274,23 @@ namespace Naos.AWS.S3
         {
             var identifierType = typeof(string).ToRepresentation();
 
-            operation.RecordFilter.DeprecatedIdTypes.MustForArg(
-                          Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.DeprecatedIdTypes)}"))
-                     .BeNull();
-            operation.RecordFilter.IdTypes.Single().MustForArg(
-                          Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.IdTypes)}"))
-                     .BeEqualTo(identifierType);
-            operation.RecordFilter.InternalRecordIds.MustForArg(
-                          Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.InternalRecordIds)}"))
-                     .BeNull();
-            operation.RecordFilter.ObjectTypes.MustForArg(
-                          Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.ObjectTypes)}"))
-                     .BeNull();
-            operation.RecordFilter.Tags.MustForArg(
-                          Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.Tags)}"))
-                     .BeNull();
-            operation.RecordFilter.Ids.MustForArg(
-                          Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.Ids)}"))
-                     .BeNull();
+            operation.RecordFilter.DeprecatedIdTypes.MustForArg(Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.DeprecatedIdTypes)}")).BeNull();
+            operation.RecordFilter.IdTypes.Single().MustForArg(Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.IdTypes)}")).BeEqualTo(identifierType);
+            operation.RecordFilter.InternalRecordIds.MustForArg(Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.InternalRecordIds)}")).BeNull();
+            operation.RecordFilter.ObjectTypes.MustForArg(Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.ObjectTypes)}")).BeNull();
+            operation.RecordFilter.Tags.MustForArg(Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.Tags)}")).BeNull();
+            operation.RecordFilter.Ids.MustForArg(Invariant($"{nameof(operation)}.{nameof(operation.RecordFilter)}.{nameof(operation.RecordFilter.Ids)}")).BeNull();
 
             var resourceLocator = this.TryGetSingleLocator(operation);
+
             Func<Task<ICollection<CloudFile>>> listFilesAsyncFunc =
                 () => this.fileManager.ListFilesAsync(resourceLocator.Region, resourceLocator.BucketName);
+
             var listedFiles = listFilesAsyncFunc.ExecuteSynchronously();
 
             var result = listedFiles
-                        .Select(_ => new StringSerializedIdentifier(_.KeyName, identifierType))
-                        .ToList();
+                .Select(_ => new StringSerializedIdentifier(_.KeyName, identifierType))
+                .ToList();
 
             return result;
         }
@@ -285,15 +323,35 @@ namespace Naos.AWS.S3
             throw new NotImplementedException();
         }
 
-        /// <inheritdoc />
-        public override IStreamRepresentation StreamRepresentation => new StreamRepresentation(this.Name);
-
         private S3ResourceLocator TryGetSingleLocator(
             ISpecifyResourceLocator operation = null)
         {
-            var resourceLocator =
-                (S3ResourceLocator)(operation?.SpecifiedResourceLocator ?? this.ResourceLocatorProtocols.Execute(new GetAllResourceLocatorsOp()).Single());
-            return resourceLocator;
+            var resourceLocator = operation?.SpecifiedResourceLocator ??
+                                  this.ResourceLocatorProtocols.Execute(new GetAllResourceLocatorsOp()).Single();
+
+            var result = (S3ResourceLocator)resourceLocator;
+
+            return result;
+        }
+
+        private IReadOnlyCollection<NamedValue<string>> GetTags(
+            IReadOnlyDictionary<string, string> userDefinedMetadata)
+        {
+            var result = new List<NamedValue<string>>();
+
+            foreach (var metadataKey in userDefinedMetadata.Keys)
+            {
+                if (metadataKey.StartsWith(MetadataKeyTagNamePrefix))
+                {
+                    var metadataValue = userDefinedMetadata[metadataKey];
+
+                    var tag = this.tagSerializer.Deserialize<NamedValue<string>>(metadataValue);
+
+                    result.Add(tag);
+                }
+            }
+
+            return result;
         }
     }
 }
